76ac4fb89ccb480a25ae19c8137e1c5fe3b604f6
Calixte <tchicoulo@hotmail.fr>
2023-02-27 17:57:13 +0100

    Added TU + TI Get Bills on bills page

--- bill-app/Billed-app-FR-Front-main/src/__tests__/Bills.js
+++ bill-app/Billed-app-FR-Front-main/src/__tests__/Bills.js
@@ -9,7 +9,7 @@ import userEvent from "@testing-library/user-event";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import mockStore from "../__mocks__/store";
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
@@ -46,27 +46,88 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted);
     });
 
-    // test("click on icon eye", async () => {
-    //   const billlllls = new Bills({
-    //     document,
-    //     onNavigate,
-    //     store: null,
-    //     localStorage: window.localStorage,
-    //   });
+    describe("When I click on an icon eye on a bill", () => {
+      test("Then, It should open a modal with a proof ", () => {
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({ type: "Employee" })
+        );
+        window.onNavigate(ROUTES_PATH.Bills);
 
-    //   const handleClickIconEye1 = jest.fn((e) =>
-    //     billlllls.handleClickIconEye(e)
-    //   );
+        const billObj = new Bills({
+          document,
+          onNavigate,
+          store: null,
+          localStorage: window.localStorage,
+        });
 
-    //   document.body.innerHTML = BillsUI({ data: bills });
+        // BootStrap Jquery modal
+        $.fn.modal = jest.fn();
 
-    //   const iconEye = screen.getAllByTestId("icon-eye");
-    //   iconEye[0].addEventListener("click", handleClickIconEye1);
-    //   userEvent.click(iconEye[0]);
-    //   expect(handleClickIconEye1).toHaveBeenCalled();
+        const handleClickIconEye = jest.fn(billObj.handleClickIconEye);
 
-    //   //to-do write expect expression
-    //   /*expect(windowIcon.classList.contains("active-icon")).toEqual(true);*/
-    // });
+        const iconEye = screen.getAllByTestId("icon-eye");
+
+        iconEye.forEach((icon) => {
+          icon.addEventListener("click", () => handleClickIconEye(icon));
+          // User interaction testing library
+          userEvent.click(icon);
+        });
+        const fileModal = document.getElementById("modaleFile");
+        expect(fileModal).toBeTruthy();
+
+        expect(handleClickIconEye).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+// test intégration GET Bills
+
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to bills", () => {
+    test("fetches bills from mock API GET", async () => {
+      jest.spyOn(mockStore, "bills");
+      const store = mockStore.bills();
+      const billsList = await store.list();
+
+      //mocked list of 4 objects
+      expect(billsList.length).toBe(4);
+      expect(billsList[1].commentary).toBe("plop");
+    });
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills");
+      });
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 404"));
+            },
+          };
+        });
+        document.body.innerHTML = BillsUI({ error: "Erreur 404" });
+        const message = screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 500"));
+            },
+          };
+        });
+
+        document.body.innerHTML = BillsUI({ error: "Erreur 500" });
+        const message = screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
+      });
+    });
   });
 });
